159115f18e6e2dbe86a48b9743288500e68b8d2a
draoshen <rafa4apple@gmail.com>
2021-05-03 17:39:57 +0200

    ya van los mandatos externos

--- main.c
+++ main.c
@@ -25,6 +25,11 @@
 //includes adicionales
 #include <signal.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+
 
 extern int obtain_order();		/* See parser.y for description */
 
@@ -37,6 +42,11 @@ int main(void)
 	char *filev[3] = { NULL, NULL, NULL };
 	int bg;
 	int ret;
+	//voy a guardar la cantidad de argumentos para cada mandato
+	int numArgs[200]; //no creo que me vayan a pasar más de 200 mandatos de una tirada y de ser así GG!
+
+
+
 
 	//Manejo de senales
 	struct sigaction act;
@@ -46,6 +56,13 @@ int main(void)
 	sigaction(SIGQUIT,&act,NULL);
 
 
+	//Funcion que va a analizar las distintas tareas que tenga que hacer
+/*	void functionName(ParameterList)
+{
+	//Statement
+	    
+}
+*/
 	setbuf(stdout, NULL);			/* Unbuffered */
 	setbuf(stdin, NULL);
 
@@ -62,28 +79,76 @@ int main(void)
  * PARA DAR UNA IDEA DE COMO UTILIZAR LAS ESTRUCTURAS
  * argvv Y filev. ESTAS LINEAS DEBERAN SER ELIMINADAS.
  */
-		for (argvc = 0; (argv = argvv[argvc]); argvc++) {
-			for (argc = 0; argv[argc]; argc++){
-				if (strstr(argv[argc],"cd")!=NULL)
-				{
-				printf("Aquí voy a tener que hacer un cd\n");
-					printf("cd ->%s\n",argv[argc]+3);
-				}
-				//printf("cd -> .\n");
-				printf("%s ", argv[argc]);
-				}
+	for (argvc = 0; (argv = argvv[argvc]); argvc++) {
+		for (argc = 0; argv[argc]; argc++){}
+				//printf("El argumento %d , es : %s \n", argc,argv[argc]);
+			numArgs[argvc]=numArgs[argvc]++;
+			//printf("\n");
+		}
+	if (filev[0]) printf("< %s\n", filev[0]);/* IN */
+	if (filev[1]) printf("> %s\n", filev[1]);/* OUT */
+	if (filev[2]) printf(">& %s\n", filev[2]);/* ERR */
+	if (bg) {
+		printf("&\n");
+		printf("El programa se ejecuta en background\n");
+	}
+	//printf("Los valores de argvc:%d,argc:%d\n",argvc,argc );
 
-				
-				printf("\n");
+	if (argvc>1)
+	{
+		//printf("nos va a tocar pringar las manos\n");
+	}
+	else{
+		//printf("nos ha tocado lo fácil\n");
+		//printf("El comando a ejecutar es:%s\n",*argvv[0] );
+		argv=argvv[0];
+		//printf("su primer argumento es: %s\n",argv[1] );
+		if (strstr(argv[0],"exit"))
+		{
+				exit(0);
+		}
+
+		if (strstr(argv[0],"cd"))
+		{
+			if (argv[1]!=NULL)
+			{
+				chdir(argv[1]);
 			}
-			
-			
-		/*
-		if (filev[0]) printf("< %s\n", filev[0]);// IN 
-		if (filev[1]) printf("> %s\n", filev[1]);// OUT
-		if (filev[2]) printf(">& %s\n", filev[2]);// ERR 
-		if (bg) printf("&\n");
-		*/
+			else{
+				chdir(getenv("HOME"));
+			}
+		}
+
+		//Mandatos no reconocidos y que por tanto son externos
+		else{
+			int pid;
+			int status;
+			switch(pid=fork()){
+				case -1:
+				perror("Error en la llamada al fork");
+				break;
+				//código del hijo
+				case 0:
+					//printf("Hola soy el hijo\n");
+					sleep(1);
+					execvp(argv[0],&argv[0]);
+					perror("Error en el exec");
+				break;
+				//código del padre
+				default:
+					//printf("Estamos esperando a que el hijo haga sus cositas\n");
+					pid=wait(&status);
+					if ( WIFEXITED(status) ){
+						int exit_status = WEXITSTATUS(status);        
+        				//printf("Exit status of the child was %d\n",exit_status);
+					}
+					//printf("He terminado con la tarea\n");
+			}
+		}
+		
+
+	}
+	//printf("%s\n",*argvv[1] );
 /*
  * FIN DE LA PARTE A ELIMINAR
  */
